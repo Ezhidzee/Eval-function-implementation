d4b45c643c5a42d642598c54c93d1ec10df492f2
Ezhidze <atrem2004atrem@gmail.com>
2022-07-29 01:34:34 +0300

    imported func strcpy_s

--- main.cpp
+++ main.cpp
@@ -1,7 +1,7 @@
 #include "Eval.cpp"
-#include <stdio.h>
+#include <cstdio>
 
 int main() {
-    printf("%.5f", eval("7.64**2 + (556.654 % 46-2.654)* 4**6")); //8250.36960
+    printf("%.5Lf", eval("7.64**2 + (556.654 % 46-2.654)* 4**6")); //8250.36960
 }
 
